cf56cc6120e7ee1dddce4a528e95a6a6dc4c7e48
Darshanhegdejaji <159263100+Darshanhegdejaji@users.noreply.github.com>
2026-05-18 16:03:39 +0530

    chore: add src/main/java/com/example/due_test_4/dto/CreateDepartmentAdminRequest.java

---
+++ src/main/java/com/example/due_test_4/dto/CreateDepartmentAdminRequest.java
@@ -0,0 +1,70 @@
+package com.example.due_test_4.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder()
+@Data()
+public class CreateDepartmentAdminRequest {
+
+    private String id;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    private String firstName;
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    private String lastName;
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    private String email;
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    private String phoneNumber;
+
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public CreateDepartmentAdminRequest() {
+    }
+
+    public CreateDepartmentAdminRequest(String id, String firstName, String lastName, String email, String phoneNumber) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+}
